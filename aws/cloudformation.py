515265a2d51c7cfe9b0954cea1cc466e3ff2c4a3
Jordi Franco <jordi.franco@scmspain.com>
2017-03-04 01:24:32 +0100

    fix cloudformation init

--- aws/cloudformation.py
+++ aws/cloudformation.py
@@ -4,21 +4,12 @@ from aws.boto_connections import AWSBotoAdapter
 
 class Cloudformation(object):
 
-    def __init__(self, account_name, environment, number, resource, role):
-        self.__environment = environment
-        self.__account_name = account_name
-        self.__profile = self.__account_name + "-" + self.__environment
+    def __init__(self, profile, resource, name):
+        self.__profile = profile
         self.__connection = AWSBotoAdapter()
         self.__resource = resource
-        self.__number = number
-        self.__role = role
-        self.__stackName = self.__set_stackName()
-
-    def __set_stackName(self):
-        self.__stackName = self.__environment + "-" + self.__role
-        if self.__environment == 'pre':
-            self.__stackName = "{}-{}".format(self.__stackName, self.__number)
-        return self.__stackName
+        self.__name = name
+        self.__stackName = self.__name
 
     def get_stackName(self):
         return self.__stackName
