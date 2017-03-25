86f55e4814dc6d819be2f230a4d23c3eb73eca6c
Jordi Franco <jordi.franco@scmspain.com>
2017-03-26 00:10:49 +0100

    debug

--- aws/ec2_adapter.py
+++ aws/ec2_adapter.py
@@ -20,9 +20,7 @@ class Ec2Adapter:
                 instance_dict = {}
                 instance_dict['public_ip'] = instance['Instances'][0]['PublicIpAddress']
                 for key in instance['Instances'][0]['Tags']:
-                    print key
-                    if 'Name' in key.keys():
-                        print("lol")
+                    print key['Name']
                 exit(1)
                 #instance_dict['name'] = instance['Instances'][0]['Tags'][0]['Name']
                 public_ips.append(instance_dict)
